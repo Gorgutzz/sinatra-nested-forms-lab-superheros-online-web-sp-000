d4410055c585793e7feecfa1e4efedb85c5fc33a
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-06-19 23:55:02 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,20 +5,18 @@ class App < Sinatra::Base
     set :views, Proc.new { File.join(root, "../views/") }
 
     get '/' do
-        erb :index
+      erb :super_hero
     end
 
     post '/teams' do
-        # binding.pry
-        @team = Team.new(params[:team])
-        #binding.pry
-        params[:team][:members].each do |details|
-            SuperHero.new(details)
-        end
-        @members = SuperHero.all
+      @team = Team.new(params[:team])
 
-        erb :team
+      params[:team][:members].each do |details|
+        SuperHero.new(details)
+      end
+      @super_heroes = SuperHero.all
 
+      erb :team
     end
 
 end
